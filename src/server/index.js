3b4677bb54ebe2629b048e72aef55e41a8e1509c
SawaGawlau <47000341+SawaGawlau@users.noreply.github.com>
2022-06-14 16:02:38 +0200

    update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,25 +1,48 @@
 const WebSocket = require('ws')
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use('/', express.static(path.resolve(__dirname, '../client')))
+
+const server = app.listen(9876)
 
 const wss = new WebSocket.Server({
-    port: 9876
+    server: server
 })
 console.log('websocket ready')
     // whenever there is a websocket connection
     // we have ws for each partivcular machine/client/server
+
+    const clients = []
     wss.on('connection', function(ws) {
-        console.log('hello from the server')
+
+        // whenever there is connection we push it to a clients array
+        // whenever there is a message we send it to all connections
+        clients.push(ws)
+        // console.log('hello from the server')
         // i want to listen on a message and send it to a server
         ws.on('message', function(data) {
             // console.log(data) // <Buffer 22 68 65...>
             // data should be send as JSON.stringify(message)
             const dataToString = JSON.parse(data) // on a frontend ws.send(JSON.stringify('hello server, whats up ?'))
-            console.log(`message from ws: ${dataToString}`)
             // whenever there is connection, we are listening for an event = mesage
             // on for this particular websocket we are going send that data back
-            ws.send(dataToString)
+            wss.clients.forEach(function each(client) {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(dataToString)
+                }
+            })
         })
     })
+/* WebSocket.readyState - 
+CONNECTING: 0
+OPEN: 1
+CLOSING: 2
+CLOSED: 3
 
+
+*/
 // console.log(wss)
 
 
