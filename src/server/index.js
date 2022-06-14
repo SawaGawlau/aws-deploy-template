9935f20540188a82d07019a6f3e09549813a368a
SawaGawlau <47000341+SawaGawlau@users.noreply.github.com>
2022-06-14 13:01:57 +0200

    websockets update
    ws.on('message')
    ws.on('connection')

--- src/server/index.js
+++ src/server/index.js
@@ -10,10 +10,13 @@ console.log('websocket ready')
         console.log('hello from the server')
         // i want to listen on a message and send it to a server
         ws.on('message', function(data) {
-            console.log(data) // <Buffer 22 68 65...>
-            // data is send as JSON.stringify(message)
+            // console.log(data) // <Buffer 22 68 65...>
+            // data should be send as JSON.stringify(message)
             const dataToString = JSON.parse(data) // on a frontend ws.send(JSON.stringify('hello server, whats up ?'))
-            console.log(dataToString)
+            console.log(`message from ws: ${dataToString}`)
+            // whenever there is connection, we are listening for an event = mesage
+            // on for this particular websocket we are going send that data back
+            ws.send(dataToString)
         })
     })
 
--- ws/websockets.md
+++ ws/websockets.md
@@ -3,3 +3,14 @@ window object has a property WebSocket:
 window.WebSocket => this is a function which allows you to pass the url and allows websocket connection
 
 why do we need ws?
+
+```
+> const ws = new WebSocket(`ws://127.0.0.1:9876`)
+
+> ws.onmessage = function(event) {
+  console.debug("WebSocket message received:", event);
+};
+
+> ws.send(JSON.stringify('hello server, whats up ?'))
+
+```
