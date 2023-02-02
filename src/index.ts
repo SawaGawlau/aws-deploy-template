29b95f45a8674011571ccd5321c3f1971bc068d7
SawaGawlau <sawa.gawlau@gmail.com>
2023-02-02 13:08:39 +0100

    test: change port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Application } from 'express'
 
 const app: Application = express()
 
-const port: number = 8080
+const port: number = 3000
 
 app.listen(port, () => {
     console.log(`Start of the application on port ${port}`)
