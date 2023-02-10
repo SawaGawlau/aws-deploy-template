5f4b7b43eb1a824358e38911c16eed1b2cfd3c3f
SawaGawlau <sawa.gawlau@gmail.com>
2023-02-10 08:29:29 +0100

    test

--- app.ts
+++ app.ts
@@ -1,8 +1,12 @@
-import express, { Application } from 'express'
+import express, { Application, Request, Response } from 'express'
 
 const app: Application = express()
 
 const port: number = 3000
+
+app.use('/', (req: Request, res: Response) => {
+  res.send('Hello world')
+})
 
 app.listen(port, () => {
     console.log(`Start of the application on port ${port}`)
